7c109e87e51c819bb7cd8f5835400db3dd2078d1
Fernando Moreira da Silva Filho <fernandomsf@id.uff.br>
2021-02-03 15:10:31 -0300

    Exercicio 3 feito.

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20201/exercicios/exercicio3/FernandoFilho/MDC.java
@@ -0,0 +1,24 @@
+package uff.ic.lleme.tcc00328.s20201.exercicios.exercicio3.FernandoFilho;
+
+import java.util.Scanner;
+
+public class MDC {
+    public static void main (String [] args){
+        int x,y,r=1;
+        Scanner in1= new Scanner(System.in);
+        System.out.println("Digite o maior número para o MDC:");
+        x = in1.nextInt();
+        Scanner in= new Scanner(System.in);
+        System.out.println("Digite o menor número para o MDC:");
+        y = in.nextInt();
+        System.out.println("O MDC de Euclides entre "+x+" e "+y+" é :");
+        while(r!=0){
+            r=x%y;
+            x=y;
+            y=r;
+        }
+        System.out.println(x);
+        in1.close();
+        in.close();
+    }
+}
