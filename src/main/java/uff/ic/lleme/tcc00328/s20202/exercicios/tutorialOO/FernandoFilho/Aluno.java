913782204c9a9e7fc6df0d9677ba8a65053987d3
Fernando Moreira da Silva Filho <fernandomsf@id.uff.br>
2021-02-24 23:18:30 -0300

    ok

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOO/FernandoFilho/Aluno.java
@@ -0,0 +1,8 @@
+package uff.ic.lleme.tcc00328.s20202.exercicios.tutorialOO.FernandoFilho;
+
+public class Aluno extends Pessoa{
+    public int matricula;
+    public String unidade;
+    public Inscricao[] inscricoes;
+
+}
---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOO/FernandoFilho/Pessoa.java
@@ -0,0 +1,6 @@
+package uff.ic.lleme.tcc00328.s20202.exercicios.tutorialOO.FernandoFilho;
+
+public class Pessoa {
+    public String nome;
+    public long cpf;
+}
