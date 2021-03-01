4180265f8beac4e7f7924087f32908fa764736bf
Fernando Moreira da Silva Filho <fernandomsf@id.uff.br>
2021-03-01 14:19:40 -0300

    ok

--- src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOOCont/FernandoFilho/SistemaAcademico.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOOCont/FernandoFilho/SistemaAcademico.java
@@ -1,6 +1,5 @@
 package uff.ic.lleme.tcc00328.s20202.exercicios.tutorialOOCont.FernandoFilho;
 
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,13 +31,14 @@ class SistemaAcademico {
 
         InputStream input = new FileInputStream(nomeArquivo);
         Scanner in = new Scanner(input);
-
-        for (int i = 0; i < 5; i++) {
+        int i = 0;
+        while (in.hasNext()) {
             aluno[i] = new Aluno();
             aluno[i].setCpf(in.nextLong());
             aluno[i].setMatricula(in.nextInt());
             aluno[i].setUnidade(in.next());
             aluno[i].setNome(in.nextLine());
+            i++;
         }
         input.close();
         in.close();
@@ -49,12 +49,13 @@ class SistemaAcademico {
 
         InputStream input = new FileInputStream(nomeArquivo);
         Scanner in = new Scanner(input);
-
-        for (int i = 0; i < 5; i++) {
+        int i = 0;
+        while (in.hasNext()) {
             disciplina[i] = new Disciplina();
             disciplina[i].setCodigo(in.next());
             disciplina[i].setSigla(in.next());
             disciplina[i].setNome(in.nextLine());
+            i++;
         }
         input.close();
         in.close();
@@ -63,9 +64,9 @@ class SistemaAcademico {
     private static void carregarTurmas(String nomeArquivo) throws IOException {
         InputStream input = new FileInputStream(nomeArquivo);
         Scanner in = new Scanner(input);
-
-        for(int i=0;i<5;i++){
-            turma[i]= new Turma();
+        int i = 0;
+        while (in.hasNext()) {
+            turma[i] = new Turma();
             turma[i].setCodigo(in.next());
             turma[i].setDisciplina(disciplina[i]);
             in.next();
@@ -73,19 +74,19 @@ class SistemaAcademico {
             turma[i].setHorario(in.next());
             in.next();
             in.next();
+            i++;
         }
         input.close();
         in.close();
-    
-    
+
     }
 
     public static void carregarInscricoes(String nomeArquivo) throws IOException {
 
         InputStream input = new FileInputStream(nomeArquivo);
         Scanner in = new Scanner(input);
-
-        for (int i = 0; i < 5; i++) {
+        int i = 0;
+        while (in.hasNext()) {
             inscricoes[i] = new Inscricao();
             inscricoes[i].setTurma(turma[i]);
             in.next();
@@ -93,39 +94,39 @@ class SistemaAcademico {
             inscricoes[i].setAluno(aluno[i]);
             inscricoes[i].setData_matricula(in.next());
             inscricoes[i].setData_cancelamento(in.next());
+            i++;
         }
         input.close();
         in.close();
     }
 
-        private static void informarNotasDeAlunos() {
-            Scanner in = new Scanner(System.in);
+    private static void informarNotasDeAlunos() {
+        Scanner in = new Scanner(System.in);
 
-            int[] conjmat = new int[5];
-            String[] conjcod = new String[5];
+        int[] conjmat = new int[5];
+        String[] conjcod = new String[5];
 
-            for(int i=0;i<5;i++){
-                conjmat[i]=inscricoes[i].getAluno().getMatricula();
-                conjcod[i]=inscricoes[i].getTurma().getDisciplina().getCodigo();
-            }
+        for (int i = 0; i < 5; i++) {
+            conjmat[i] = inscricoes[i].getAluno().getMatricula();
+            conjcod[i] = inscricoes[i].getTurma().getDisciplina().getCodigo();
+        }
 
-            for(int i=0;i<5;i++){
-                System.out.println("Digite a nota 1 do aluno de matrícula : "+aluno[i].getMatricula()
-                        + " da turma de código : " + disciplina[i].getCodigo());
-                inscricoes[i].getNotas()[0] = in.nextFloat();
-                System.out.println("Digite a nota 2 :");
-                inscricoes[i].getNotas()[1] = in.nextFloat();
-            }
-            in.close();    
-        }   
-        private static void calcularMedias(){
-            for(int i=0;i<5;i++){
-            System.out.println("A media da matricula "+aluno[i].getMatricula() + " na disciplina de codigo "
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite a nota 1 do aluno de matrícula " + aluno[i].getMatricula()
+                    + " da turma de código : " + disciplina[i].getCodigo());
+            inscricoes[i].getNotas()[0] = in.nextFloat();
+            System.out.println("Digite a nota 2 :");
+            inscricoes[i].getNotas()[1] = in.nextFloat();
+        }
+        in.close();
+    }
+
+    private static void calcularMedias() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("A media da matricula " + aluno[i].getMatricula() + " na disciplina de codigo "
                     + disciplina[i].getCodigo());
             System.out.println((inscricoes[i].getNotas()[0] + inscricoes[i].getNotas()[1]) / 2);
         }
     }
-    
+
 }
-
-
