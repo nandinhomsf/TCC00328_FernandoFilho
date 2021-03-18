7c12faf337c209cde4db88514f708f5f3b5c7671
Fernando Moreira da Silva Filho <fernandomsf@id.uff.br>
2021-03-18 17:03:07 -0300

    Ok

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio20/FernandoFilho/Cliente.java
@@ -0,0 +1,41 @@
+package uff.ic.lleme.tcc00328.s20202.exercicios.exercicio20.FernandoFilho;
+
+public class Cliente extends Cadastro {
+    private int periodo;
+    private int quarto1;
+    private int quarto3;
+    private int quarto2;
+
+    public int getPeriodo() {
+        return this.periodo;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    public int getQuarto1() {
+        return this.quarto1;
+    }
+
+    public void setQuarto1(int quarto1) {
+        this.quarto1 = quarto1;
+    }
+
+    public int getQuarto3() {
+        return this.quarto3;
+    }
+
+    public void setQuarto3(int quarto3) {
+        this.quarto3 = quarto3;
+    }
+
+    public int getQuarto2() {
+        return this.quarto2;
+    }
+
+    public void setQuarto2(int quarto2) {
+        this.quarto2 = quarto2;
+    }
+
+}
